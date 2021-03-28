04a39356a38aa263b7990a11be7b075b6402b6e5
bal0229 <bal29alba@gmail.com>
2021-03-28 11:38:16 -0400

    phaser eliminar impor innecesario

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-import { Game4 } from './game4.js';
-import { Game5 } from './game5.js';
-import {Game6}from './game6.js'
 import {Game7}from './game7.js'
-import {Game8} from './game8.js'
-import {Game3}from './game3.js';
 import { Nivel1 } from './Nivel1.js';
 import {Boot} from './Boot.js';
 import { Instructions } from "./Instructions.js";
 import { StackerGame } from "./StackerGame.js";
 import { GameOver } from "./GameOver.js";
 import {Nivel2} from "./Nivel2.js";
-import {Nivel3} from "./Nivel3.js";
-import {Animales} from "./Animales.js";
-import { Octopus } from './octopus.js';
 import { GameOver2 } from './GameOver2.js';
 import { GameOver3 } from './GameOver3.js';
 import { GameOver4 } from './GameOver4.js';
